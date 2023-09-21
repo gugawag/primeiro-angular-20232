c366902f9f3d776a6ff9ba03d165d9699f5cc951
Gustavo Wagner <gugawag@gmail.com>
2023-09-21 18:32:37 -0300

    v1-alunos em Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Aluno} from "./shared/model/aluno";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'primeiro-angular-20232';
+  alunos: Aluno[] = [];
+  alunoTratamento: Aluno;
+
+  constructor() {
+    this.alunoTratamento = new Aluno('', '', 0);
+  }
+
+  cadastrar(): void {
+    this.alunos.push(this.alunoTratamento);
+    this.alunoTratamento = new Aluno('', '', 0);
+  }
+
+  remover(alunoARemover: Aluno): void {
+    const indxARemover = this.alunos.findIndex(aluno =>
+      aluno.matricula === alunoARemover.matricula);
+
+    if (indxARemover >= 0) {
+      this.alunos.splice(indxARemover, 1);
+    }
+
+  }
+
 }
